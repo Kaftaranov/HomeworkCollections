0d5fbabe22683485e412cfcfac2960bbc341ab8b
Timur Kaftaranov <kaftaranov@rambler.ru>
2022-07-01 20:06:31 +0600

    Initial commit

---
+++ src/main/java/pro/sky/homework/HomeworkCollections/HomeworkCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homework.HomeworkCollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkCollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/homework/HomeworkCollections/HomeworkCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework.HomeworkCollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
